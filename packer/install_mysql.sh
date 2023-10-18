bcc1b9e1baa4140fd26103aa05e0f3cc849b2e98
Vladlen <vladlen.bondarchuk@gmail.com>
2023-10-18 13:32:41 +0300

    test-dz10

---
+++ packer/install_mysql.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+MYSQL_USER="admin"
+MYSQL_PASSWORD="Pa55WD"
+MYSQL_DB="flask_db"
+
+# Обновляем список доступных пакетов
+sudo yum update -y
+
+# Установка MariaDB Server
+sudo yum install -y mariadb105-server
+
+# Запуск MariaDB и включение автозапуска при загрузке
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+sudo bash -c 'echo "[mysqld]" >> /etc/my.cnf'
+sudo bash -c 'echo "bind-address = 0.0.0.0" >> /etc/my.cnf'
+sudo systemctl restart mysql
+
+
+# Создаем пользователя и базу данных MySQL
+sudo mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+USE mysql;
+SELECT user FROM user WHERE user = '$MYSQL_USER';
+CREATE DATABASE IF NOT EXISTS $MYSQL_DB;
+GRANT ALL ON $MYSQL_DB.* TO '$MYSQL_USER'@'%';
+FLUSH PRIVILEGES;"
+
+# Проверяем список доступных баз данных
+sudo mysql -e "SHOW DATABASES;"
+
+# Устанавливаем дополнительные пакеты
+sudo yum install -y python3-pip 
+
+# Очищаем кэш пакетов и освобождаем дисковое пространство (необязательно)
+sudo yum clean all
+
+
+
